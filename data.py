2c42b69efc45596dab2ce93ce10262df68055689
riteshmgr557 <56671522+riteshmgr557@users.noreply.github.com>
2019-12-28 21:31:27 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,116 @@
+import discord
+
+def  init():
+    global bot_token
+    global self_bot_token
+    global message
+    global embed
+    global output_channel
+    global input_channels
+    global command_channel
+
+    global game_in_session
+    global counter_public_1
+    global counter_public_2
+    global counter_public_3
+    global counter_private_1
+    global counter_private_2
+    global counter_private_3
+    global counter1
+    global counter2
+    global counter3
+    global weight
+    global weight_time
+    global seconds_elapsed
+
+bot_token = 'NjU5MjUxMDUyNjU4NTU2OTQ1.Xgd3oA.a6yNwUw0qyEDdiPkcMxe5GdUj0s'
+self_bot_token = 'NjExMDQ4NzU2OTUxMTIxOTIw.XbRgsw.Heu7k-M0qZpH6a0CgbS_0nIF-ZU'
+
+message = None
+embed = None
+embed_best = None
+
+#trivia-answers
+output_channel = discord.Object(id= '659785967112028161')
+
+input_hq_private  = [
+    "569420198717816852",
+    "459842150323060736",
+    "523359669280833536",
+	"535628205139296256",
+	"525131707410677761",
+	"513818250652680213",
+	"544381529829146645",
+    "569843705654149130",
+    "566979656083963918", # answers1
+    "559442345674670082", #answers2
+    "559357612068700183", #trivia-answers
+    "656530625708425216",
+"656045760089030678",
+"656530625708425216",
+"659336481688518666",
+"655058181583667201",
+"659298884279074846",
+"656424018047664158",
+"658757043343065138",
+"654684108588580884",
+"654337758285791255",
+"657955269086674955",
+"641656808129167371",
+"613746114016968806",
+"595654063870181386",
+"594148529044324362",
+"599078226131681320",
+"602383647718375434",
+"596248827460190208",
+"601394440644460584", "642925795215671296",
+"641624623133491200",
+"640758301683875851",
+"569420128794443776",
+"640767742378770452",
+"630614554757431297",
+"640768412704047163",
+"595654063870181386",
+"639111321023021082",
+"640768499848839209",
+"638695271152222208",
+"638670943174131713",
+"638696089393954817",
+"586848830075568147",
+"644376326437994529",
+"638797953833893894",
+"640767742378770452"
+"546237713637048331",
+"569420128794443776",
+"643460816481419284",
+"598485996169854997",
+"598486157440843786",
+"644158799464103936",
+"640767819587518484",
+"640768412704047163",
+"639111321023021082",
+"654343474451316746",
+"601676426915348483",
+"654337758285791255"
+]
+input_hq_public = ['659785967112028161']
+command_channel = '659785967112028161' #trivia-answers
+admin_chat = '659785967112028161' # answers2
+
+game_in_session = False
+counter_public_1 = 0
+counter_public_2 = 0
+counter_public_3 = 0
+counter_private_1 = 0
+counter_private_2 = 0
+counter_private_3 = 0
+counter1 = 0
+counter2 = 0
+counter3 = 0
+weight = 5
+weight_time = 1
+wronggone1 = 0
+wronggone2 = 0
+wronggone3 = 0
+
+seconds_elapsed = 0
